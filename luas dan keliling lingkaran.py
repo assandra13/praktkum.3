a734043d6e1b79932e1b7a7a6145ae32999422ff
assandra13 <ahmeng965@gmail.com>
2022-11-02 09:51:00 +0700

    latihan 3

---
+++ luas dan keliling lingkaran.py
@@ -0,0 +1,20 @@
+print("Assandra Julyant Firdausy")
+print(312210368)
+
+# luas dan keliling lingkaran
+d = float(input("masukan diameter lingkaran :")) 
+r = (d/2)
+phi = 3.14
+L = phi *(r*r)
+K = (phi*d)
+print("diketahui :")
+print("diameter =",d,"cm")
+print("jari-jari",r,"cm")
+print("phi =",phi)
+print("ditanya luas lingkaran dan keliling lingkaran ?")
+print(" 1 = phi x r x r ")
+print(" L =",phi," x ",r," x ",r)
+print(" K = phi x d")
+print(" K = ",phi," x ",d)
+print("luas lingkaran adalah = ",L,"cm")
+print("keliling lingkaran adalah :",K,"cm")
---
+++ main.py
@@ -0,0 +1,7 @@
+print("program menghitung luas dan keliling lingkaran")
+r = int(input("masukan jari jari = "))
+
+phi = 3.14
+r = int(input("masukan panjang jari jari lingkaran = "))
+kl = 2*phi*r
+print("keliling lingkaran = ,kl")
